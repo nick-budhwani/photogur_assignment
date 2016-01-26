d6fd042906a5261f4ae3754098653c68580cc0e4
nadir budhwani <deniro0@yahoo.com>
2016-01-26 05:55:55 -0500

    Adding features to edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
   Rails.application.routes.draw do
     root 'pictures#index'
-    
+
     get 'pictures' => 'pictures#index'
+
+    get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+    patch 'pictures/:id' => "pictures#update"
 
     post 'pictures' => 'pictures#create' # this is a new line of code
     get 'pictures/new' => 'pictures#new' # this is also a new line of code
